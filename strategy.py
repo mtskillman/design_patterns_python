c1726466877061426d2d729fc7c98ddb37673d95
Matt Skillman <mtskillman@gmail.com>
2020-02-23 18:26:36 -0500

    finished strategy basic example

---
+++ strategy.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def __init__(self, behavior):
+        self.behavior = behavior
+
+    def move(self):
+        self.behavior()
+
+
+class Car(Vehicle):
+    def honk(self):
+        print('honk')
+
+
+class Plane(Vehicle):
+    pass
+
+
+if __name__ == "__main__":
+    for Foo in [Car(lambda: print("drive")), Plane(lambda: print("fly"))]:
+        Foo.move()
+        if isinstance(Foo, Car):
+            Foo.honk()
